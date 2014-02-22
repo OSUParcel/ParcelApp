73a70ea47aa1a2dfa6fdeba3c14d05866b924d03
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-02-22 12:20:01 -0800

    unique device id

---
+++ ParcelApp/PARDeviceID.m
@@ -0,0 +1,39 @@
+//
+//  PARDeviceID.m
+//  ParcelApp
+//
+//  Created by Cezary Wojcik on 2/22/14.
+//  Copyright (c) 2014 Cezary Wojcik. All rights reserved.
+//
+
+#import "PARDeviceID.h"
+
+@implementation PARDeviceID
+
+@synthesize deviceID;
+
+- (PARDeviceID*)init
+{
+    self = [super init];
+    if (self) {
+        // set default values
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup
+{
+    if (![[NSUserDefaults standardUserDefaults] objectForKey:DEVICE_ID_KEY]) {
+        // create new device key
+        CFUUIDRef identifier = CFUUIDCreate(NULL);
+        self.deviceID = (NSString*)CFBridgingRelease(CFUUIDCreateString(NULL, identifier));
+        NSLog(@"%@", self.deviceID);
+        [[NSUserDefaults standardUserDefaults] setObject:self.deviceID forKey:DEVICE_ID_KEY];
+    } else {
+        // recall old device key
+        self.deviceID = [[NSUserDefaults standardUserDefaults] objectForKey:DEVICE_ID_KEY];
+    }
+}
+
+@end
