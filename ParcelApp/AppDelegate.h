03a67a74ab23d76c6646481e2f7725e1a6118843
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-02-22 09:54:47 -0800

    Initial Commit

---
+++ ParcelApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParcelApp
+//
+//  Created by Cezary Wojcik on 2/22/14.
+//  Copyright (c) 2014 Cezary Wojcik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
